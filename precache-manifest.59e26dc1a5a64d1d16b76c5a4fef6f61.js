3d63c06230d591152d466c1345191bbd44477155
DiogoLuxa <diogoluxa@outlook.com>
2020-04-16 02:09:49 -0300

    Updates

---
+++ precache-manifest.59e26dc1a5a64d1d16b76c5a4fef6f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81667cc1078e7286dc74a48a1aef754",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "36f1b8c8841027c074f1",
+    "url": "/React-todo-list/static/css/main.a1a69b16.chunk.css"
+  },
+  {
+    "revision": "3e021290a8ad533e839a",
+    "url": "/React-todo-list/static/js/2.a7565f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list/static/js/2.a7565f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f1b8c8841027c074f1",
+    "url": "/React-todo-list/static/js/main.82f8cbff.chunk.js"
+  },
+  {
+    "revision": "93c1ae1ce2172487173d",
+    "url": "/React-todo-list/static/js/runtime-main.1e617108.js"
+  }
+]);
